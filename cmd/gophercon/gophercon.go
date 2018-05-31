6ab5f93a6173c47b8567cda81f8641851696c95e
Hrafn Jökull Geirsson <hrafng@ccpgames.com>
2018-05-31 14:25:42 +0000

    Running in a container now.

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/hrafn/gophercon2018/pkg/routing"
 	"github.com/hrafn/gophercon2018/pkg/webserver"
@@ -16,6 +18,8 @@ func main() {
 		"Service is starting, version is %s, commit is %s, time is %s...",
 		version.Release, version.Commit, version.BuildTime,
 	)
+	
+	shutdown := make(chan error, 2)
 
 	// you can also use github.com/kelseyhightower/envconfig
 	// to keep your config more structured
@@ -23,9 +27,45 @@ func main() {
 	if len(port) == 0 {
 		log.Fatal("Service port wasn't set")
 	}
-
+	
 	r := routing.BaseRouter()
 	ws := webserver.New("", port, r)
-
-	log.Fatal(ws.Start())
+	
+	go func() {
+	    err := ws.Start()
+		shutdown <- err
+	}()
+	
+	internalPort := os.Getenv("INTERNAL_PORT")
+	if len(internalPort) == 0 {
+		log.Fatal("Internal port wasn't set")
+	}
+	
+	diagnosticsRouter := routing.DiagnosticsRouter()
+	diagnosticsServer := webserver.New(
+	    "", internalPort, diagnosticsRouter,
+	)
+	
+	log.Fatal(diagnosticsServer.Start())
+	
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	
+	select {
+	    case killSignal := <-interrupt:
+			log.Printf("Got %s. Stopping", killSignal)
+		case err := <-shutdown:
+            log.Printf("Got an error '%s'. Stopping...", err)
+	}
+	err := ws.Stop()
+	if err != nil {
+		log.Print(err)
+	}
+	err = diagnosticsServer.Stop()
+	if err != nil {
+		log.Print(err)
+	}
+	
+	// stop extra tasks...
+	log.Print("Service was stopped.")	
 }
